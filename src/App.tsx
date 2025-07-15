996a4a3bb6952b0c7f606ccc2c2191529359bfd3
Adarsh Kumar Singh <145589188+Adarsh0710@users.noreply.github.com>
2025-07-15 08:14:42 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -846,7 +846,7 @@ const Footer = () => (
         <div className="md:col-span-2">
           {/* Image Logo */}
           {/* <img src={panIndiaLogo} alt="PanIndia Logistics Logo" className="h-20 mb-6" /> */}
-          <img src="panindia.jpg" alt="PanIndia Logo" className="h-20 mb-6" />
+          <img src="/assets/panindia.jpg" alt="PanIndia Logo" className="h-20 mb-6" />
 
 
           <p className="text-lg text-gray-300 leading-relaxed mb-6">
